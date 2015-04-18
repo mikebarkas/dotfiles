224bdbbe592963db2c6daf6997867d9eb9340e35
Mike Barkas <mike@barkas.com>
2015-04-18 17:40:52 -0400

    Added Drupal code sniff alias.

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -4,17 +4,16 @@
 alias vim='/usr/local/Cellar/vim/7.4.488/bin/vim'
 alias gl9='glog -n9'
 
-#
 # Mac
-#
-
 # Show/hide hidden files.
 alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-
 # Hide/show desktop icons.
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Drupal
+alias dcs="phpcs --standard=Drupal,DrupalPractice --extensions='php,module,inc,install,test,profile,theme,js,css,info,txt' --ignore='*.features.*.inc'"
 
 #
 # Config LESS output.
