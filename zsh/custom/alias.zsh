785acbc951506db48c2cc37728e549a4135cdfa8
Mike Barkas <mike@barkas.com>
2016-01-16 23:06:16 -0500

    Changed version of Vim to 7.4.1016

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,7 +1,7 @@
 #
 # Alias
 #
-alias vim='/usr/local/Cellar/vim/7.4.488/bin/vim'
+alias vim='/usr/local/Cellar/vim/7.4.1016/bin/vim'
 alias gs="git status -sb"
 alias gb="git branch -vv"
 alias gl9='glog -n9'
