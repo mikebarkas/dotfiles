e6b03ca47fcf837a6dfd43515b7721e68bc7e5c5
Mike Barkas <mike@barkas.com>
2016-06-07 16:19:12 -0400

    Create loop to backup and link dotfiles.

--- setup/setup.py
+++ setup/setup.py
@@ -3,9 +3,35 @@
 import os
 
 
+# Define ansi color.
 class color:
     red = '\033[1;31m'
+    green = '\033[1;32m'
     yellow = '\033[1;33m'
-    cyan = '\033[1;36m' 
+    cyan = '\033[1;36m'
     end = '\033[00m'
+
+dotfiles = [
+    '.testone',
+    '.testtwo',
+]
+
+os.chdir(os.path.expanduser('~'))
+
+# Backup existing files and link to dotfiles directory.
+for dotfile in dotfiles:
+    print 'Checking for existing ' + color.cyan + dotfile + color.end
+
+    if os.path.islink(dotfile):
+        print color.yellow + 'File already linked.' + color.end
+        continue
+
+    if os.path.isfile(dotfile):
+        print color.yellow + 'File found.. making backup of existing file.' + color.end
+        os.rename(dotfile, dotfile + '-original')
+
+    print 'Symlinking ' + color.green + dotfile + color.end + ' in home directory..'
+    os.symlink('/tmp/' + dotfile[1:], dotfile)
+
+    print ''
 
