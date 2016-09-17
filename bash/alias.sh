418efd5317cbb765a720983795789533e5b06b2d
Mike Barkas <mike@barkas.com>
2016-09-16 22:50:28 -0400

    Including file for aliases.

---
+++ bash/alias.sh
@@ -0,0 +1,21 @@
+#
+# Alias.
+#
+
+# System.
+alias l='ls -lGFhi'
+alias ll='ls -AlGFhi'
+alias lll='ls -AlGFhi | less'
+alias lt='ls -A1t'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Git.
+alias g='git'
+alias gco='git checkout'
+alias gcm='git checkout master'
+alias ga='git add'
+alias gb='git branch -vv'
+alias gba='git branch -vva'
+alias gr='git remote -v'
+alias gc='git commit -v'
