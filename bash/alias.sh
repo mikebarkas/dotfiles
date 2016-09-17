a8de429efaf6294e02de12be45891d4e3086832c
Mike Barkas <mike@barkas.com>
2016-09-16 23:03:19 -0400

    Update Git alias in bashrc.

--- bash/alias.sh
+++ bash/alias.sh
@@ -12,10 +12,18 @@ alias ...='cd ../..'
 
 # Git.
 alias g='git'
+alias ga='git add'
+
 alias gco='git checkout'
 alias gcm='git checkout master'
-alias ga='git add'
+
 alias gb='git branch -vv'
 alias gba='git branch -vva'
 alias gr='git remote -v'
+
 alias gc='git commit -v'
+alias gcmsg='git commit -m'
+
+alias gs='git status -sb'
+alias gcount='git shortlog -sn'
+alias gd='git diff'
