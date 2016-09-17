cdfe7e080e6421c0fc184505b54dc3bf753c8955
Mike Barkas <mike@barkas.com>
2016-09-17 16:13:51 -0400

    Include functions in a file.

---
+++ bash/functions.sh
@@ -0,0 +1,18 @@
+# Set virtaulenv.
+function set_virtualenv() {
+  if test -z "$VIRTUAL_ENV" ; then
+      VIRTUALENV=""
+  else
+      VIRTUALENV="${BLUE}[`basename \"$VIRTUAL_ENV\"`]${COLOR_NONE}"
+  fi
+}
+
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
+}
+
+# Display branch name.
+git_branch() {
+  git rev-parse --abbrev-ref HEAD 2> /dev/null
+}
+
