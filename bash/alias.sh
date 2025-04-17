2bc017577ceaef4b4eff18668f40ee44bd13df88
Mike Barkas <mikebarkas1@gmail.com>
2025-04-17 14:12:55 -0400

    32: Updates to alias file

--- bash/alias.sh
+++ bash/alias.sh
@@ -2,7 +2,9 @@
 # Alias.
 # ------------------------------------
 
+#
 # System.
+#
 alias l='ls -lGFhi'
 alias ll='ls -AlGFhi'
 alias lll='ls -AlGFhi | less'
@@ -11,26 +13,32 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 
+#
 # Vim.
+#
 alias sv="sudo vim"
 alias v="vim"
 alias v.="vim ."
 alias v,="vim ."
 
 
+#
 # Tmux.
+#
 alias tls="tmux ls"
 alias tat="tmux a -t"
 
 
+#
 # Git.
+#
 alias g='git'
 alias ga='git add'
 alias gap='git add -p'
 
 alias gco='git checkout'
 alias gcm='git checkout main'
-alias gcd='git checkout develop'
+alias gcd='git checkout dev'
 
 alias gb='git branch'
 alias gbv='git branch -vv'
@@ -41,8 +49,8 @@ alias gc='git commit -v'
 alias gcmsg='git commit -m'
 alias gp='git push'
 alias gcp='git cherry-pick'
-alias gpom='git pull origin main'
-alias gpod='git pull origin develop'
+#alias gpom='git pull origin main'
+#alias gpod='git pull origin dev'
 
 alias gs='git status -sb'
 alias gsi='git status --ignored'
@@ -56,47 +64,45 @@ alias gls='git log --oneline --graph --decorate --stat'
 alias glp='git shortlog'
 
 
-# Mercurial.
-#alias hgs="hg status"
-#alias hgp="hg push default"
-#alias hgl='hg log -G'
 
-
+#
 # Python.
+#
 alias workoff='deactivate'
 alias off='deactivate'
-alias pyman='python manage.py'
-alias hk='heroku'
 
 
+#
 # Kubernetes.
+#
 alias k='kubectl'
 alias kg='kubectl get'
+alias kd='kubectl describe'
+alias kcc='kubectl config current-context'
+alias kuc='kubectl config use-context'
 
 #
 # Docker.
 #
-#alias start-docker="source /Applications/Docker/Docker\ Quickstart\ Terminal.app/Contents/Resources/Scripts/start.sh"
 alias dk='docker'
 alias dc='docker-compose'
 alias dki='docker images'
-#alias dm='docker-machine'
 
 #
 # DDEV
 #
 alias ddd='ddev drush'
 alias ddc='ddev composer'
-alias my-drupal-check='vendor/bin/drupal-check --memory-limit=1g -d'
+#alias my-drupal-check='vendor/bin/drupal-check --memory-limit=1g -d'
 
 #
 # Terminus
 #
-alias t="terminus"
-alias trd='terminus remote:drush'
-alias dcs='phpcs --standard=Drupal,DrupalPractice --extensions='\''php,module,inc,install,test,profile,theme,js,css,info,txt'\'' --ignore='\''*.features.*.inc'\'''
+#alias t="terminus"
+#alias trd='terminus remote:drush'
+#alias dcs='phpcs --standard=Drupal,DrupalPractice --extensions='\''php,module,inc,install,test,profile,theme,js,css,info,txt'\'' --ignore='\''*.features.*.inc'\'''
 
 #
 # Ctags
 #
-alias ctags_python='ctags --fields=+l --languages=python --python-kinds=-iv -f ./tags '$(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
+#alias ctags_python='ctags --fields=+l --languages=python --python-kinds=-iv -f ./tags '$(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
